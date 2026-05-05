33698f971dc6469eb753977f8f09d822a57a1489
Robin Ace Fortes <robinacefortes@gmail.com>
2026-05-05 18:21:46 +0800

    added: umami script for views and analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,10 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <!-- Umami Analytics -->
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="a4732421-80fa-45bd-b51b-cf270ce13555"></script>
+    
     @viteReactRefresh
     @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
